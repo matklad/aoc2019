21882bcac6aebf513af8592cf77ab3fb9537218a
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-12-03 08:21:52 +0100

    Day 3

---
+++ src/bin/day3.rs
@@ -0,0 +1,136 @@
+use aoc::{Error, Result};
+
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin();
+
+    let mut path1 = String::new();
+    stdin.read_line(&mut path1)?;
+
+    let mut path2 = String::new();
+    stdin.read_line(&mut path2)?;
+
+    let res = solve_steps(&path1, &path2);
+    println!("{}", res);
+    Ok(())
+}
+
+fn _solve_dist(path1: &str, path2: &str) -> i64 {
+    let path1 = parse_path(path1).unwrap();
+    let path2 = parse_path(path2).unwrap();
+
+    let path1_points: HashSet<(i64, i64)> = trace_path(&path1).into_iter().collect();
+    let common_points = trace_path(&path2)
+        .into_iter()
+        .skip(1)
+        .filter(|it| path1_points.contains(&it));
+
+    let min_dist = common_points.map(|(x, y)| x.abs() + y.abs()).min().unwrap();
+    min_dist
+}
+
+fn solve_steps(path1: &str, path2: &str) -> usize {
+    let path1 = parse_path(path1).unwrap();
+    let path2 = parse_path(path2).unwrap();
+
+    let path1_points: HashMap<(i64, i64), usize> = trace_path(&path1)
+        .into_iter()
+        .enumerate()
+        .map(|(i, (x, y))| ((x, y), i))
+        .rev()
+        .collect();
+
+    let common_points = trace_path(&path2)
+        .into_iter()
+        .enumerate()
+        .skip(1)
+        .filter_map(|(d2, (x, y))| {
+            let d1 = *path1_points.get(&(x, y))?;
+            Some(d1 + d2)
+        });
+
+    common_points.min().unwrap()
+}
+
+fn parse_path(text: &str) -> Result<Vec<Segment>> {
+    text.trim().split(",").map(|it| it.parse()).collect()
+}
+
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+struct Segment {
+    dir: Direction,
+    len: u64,
+}
+
+impl FromStr for Segment {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Segment> {
+        let dir = match s.chars().next() {
+            Some('U') => Direction::Up,
+            Some('R') => Direction::Right,
+            Some('D') => Direction::Down,
+            Some('L') => Direction::Left,
+            _ => Err("invalid direction")?,
+        };
+        let len: u64 = s[1..].parse()?;
+        Ok(Segment { dir, len })
+    }
+}
+
+fn trace_path(segments: &[Segment]) -> Vec<(i64, i64)> {
+    let mut res = vec![(0, 0)];
+    for seg in segments.iter() {
+        let (x, y) = *res.last().unwrap();
+        let (dx, dy) = match seg.dir {
+            Direction::Up => (1, 0),
+            Direction::Right => (0, 1),
+            Direction::Down => (-1, 0),
+            Direction::Left => (0, -1),
+        };
+        res.extend((1..=(seg.len as i64)).map(|d| (x + dx * d, y + dy * d)));
+    }
+    res
+}
+
+#[test]
+fn test_examples() {
+    assert_eq!(
+        _solve_dist(
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+        ),
+        159,
+    );
+    assert_eq!(
+        _solve_dist(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        ),
+        135,
+    );
+
+    assert_eq!(
+        solve_steps(
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83"
+        ),
+        610,
+    );
+    assert_eq!(
+        solve_steps(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        ),
+        410,
+    );
+}
