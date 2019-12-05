a8583e7104128ccba39f4dec70b1328ad7c11dd7
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-12-05 07:33:56 +0100

    Move IntCode to a lib

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,6 +1,6 @@
-use std::{convert::TryInto, io::stdin};
+use std::io::stdin;
 
-use aoc::Result;
+use aoc::{IntCode, Result};
 
 fn main() -> Result<()> {
     let line = {
@@ -30,88 +30,8 @@ fn main() -> Result<()> {
 }
 
 fn run(mut mem: Vec<u64>) -> Vec<u64> {
-    Computer::new(&mut mem).run();
+    IntCode::new(&mut mem).run();
     mem
-}
-
-struct Computer<'a> {
-    mem: &'a mut [u64],
-    ip: usize,
-}
-
-impl Computer<'_> {
-    fn new(mem: &mut [u64]) -> Computer {
-        Computer { mem, ip: 0 }
-    }
-    fn run(mut self) {
-        while self.step() {}
-    }
-    fn step(&mut self) -> bool {
-        let op = self.decode();
-        match op {
-            Op::Halt => return false,
-            Op::Arith { op, lhs, rhs, dst } => {
-                let lhs = self.mem[lhs as usize];
-                let rhs = self.mem[rhs as usize];
-                self.mem[dst as usize] = op.eval(lhs, rhs);
-            }
-        }
-        self.ip += op.code_len();
-        true
-    }
-    fn decode(&self) -> Op {
-        let op_code = self.mem[self.ip];
-        match op_code {
-            1 | 2 => {
-                let op = match op_code {
-                    1 => ArithOp::Add,
-                    2 => ArithOp::Mul,
-                    _ => unreachable!(),
-                };
-                let [lhs, rhs, dst]: [u64; 3] = self.mem[self.ip + 1..self.ip + 4]
-                    .try_into()
-                    .expect("invalid arith op");
-                Op::Arith { op, lhs, rhs, dst }
-            }
-            99 => Op::Halt,
-            _ => panic!("unknown op: {}", op_code),
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-enum Op {
-    Halt,
-    Arith {
-        op: ArithOp,
-        lhs: u64,
-        rhs: u64,
-        dst: u64,
-    },
-}
-
-#[derive(Clone, Copy)]
-enum ArithOp {
-    Add,
-    Mul,
-}
-
-impl Op {
-    fn code_len(&self) -> usize {
-        match self {
-            Op::Halt => 1,
-            Op::Arith { .. } => 4,
-        }
-    }
-}
-
-impl ArithOp {
-    fn eval(&self, lhs: u64, rhs: u64) -> u64 {
-        match self {
-            ArithOp::Add => lhs + rhs,
-            ArithOp::Mul => lhs * rhs,
-        }
-    }
 }
 
 #[test]
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,84 @@
+use std::convert::TryInto;
+
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub struct IntCode<'a> {
+    mem: &'a mut [u64],
+    ip: usize,
+}
+
+impl IntCode<'_> {
+    pub fn new(mem: &mut [u64]) -> IntCode {
+        IntCode { mem, ip: 0 }
+    }
+    pub fn run(mut self) {
+        while self.step() {}
+    }
+    fn step(&mut self) -> bool {
+        let op = self.decode();
+        match op {
+            Op::Halt => return false,
+            Op::Arith { op, lhs, rhs, dst } => {
+                let lhs = self.mem[lhs as usize];
+                let rhs = self.mem[rhs as usize];
+                self.mem[dst as usize] = op.eval(lhs, rhs);
+            }
+        }
+        self.ip += op.code_len();
+        true
+    }
+    fn decode(&self) -> Op {
+        let op_code = self.mem[self.ip];
+        match op_code {
+            1 | 2 => {
+                let op = match op_code {
+                    1 => ArithOp::Add,
+                    2 => ArithOp::Mul,
+                    _ => unreachable!(),
+                };
+                let [lhs, rhs, dst]: [u64; 3] = self.mem[self.ip + 1..self.ip + 4]
+                    .try_into()
+                    .expect("invalid arith op");
+                Op::Arith { op, lhs, rhs, dst }
+            }
+            99 => Op::Halt,
+            _ => panic!("unknown op: {}", op_code),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Op {
+    Halt,
+    Arith {
+        op: ArithOp,
+        lhs: u64,
+        rhs: u64,
+        dst: u64,
+    },
+}
+
+#[derive(Clone, Copy)]
+enum ArithOp {
+    Add,
+    Mul,
+}
+
+impl Op {
+    fn code_len(&self) -> usize {
+        match self {
+            Op::Halt => 1,
+            Op::Arith { .. } => 4,
+        }
+    }
+}
+
+impl ArithOp {
+    fn eval(&self, lhs: u64, rhs: u64) -> u64 {
+        match self {
+            ArithOp::Add => lhs + rhs,
+            ArithOp::Mul => lhs * rhs,
+        }
+    }
+}
