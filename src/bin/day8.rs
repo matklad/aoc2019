201b34aba52068b8c0f3ac8ad511119350f419e3
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-12-08 09:20:53 +0100

    Day 8, part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -10,15 +10,28 @@ fn main() -> aoc::Result<()> {
     let layer_size = width * height;
     let n_layers = image.len() / width * height;
     let mut chunks = image.chunks_exact(layer_size);
-    let (chunk, _n_zeros) = chunks
-        .by_ref()
-        .map(|it| (it, count(it, b'0')))
-        .min_by_key(|(_, n_zeros)| *n_zeros)
-        .unwrap();
+    let mut res = vec![2; layer_size];
+    for chunk in chunks.by_ref().rev() {
+        for (dst, &src) in res.iter_mut().zip(chunk.iter()) {
+            if src != b'2' {
+                *dst = src
+            }
+        }
+    }
     assert!(chunks.remainder().is_empty());
-
-    let res = count(chunk, b'1') * count(chunk, b'2');
-    println!("{}", res);
+    let mut buf = String::new();
+    for row in res.chunks_exact(width) {
+        buf.clear();
+        for pixel in row.iter() {
+            let c = match pixel {
+                b'0' => '.',
+                b'1' => 'X',
+                c => panic!("uncovered pixel {}", c),
+            };
+            buf.push(c);
+        }
+        println!("{}", buf)
+    }
     Ok(())
 }
 
