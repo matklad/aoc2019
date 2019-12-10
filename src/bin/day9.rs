7d2442b40c0142317e4663c02b421d1078e68d7b
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-12-10 08:13:11 +0100

    Day 9

---
+++ src/bin/day9.rs
@@ -0,0 +1,48 @@
+use std::io::{stdin, Read};
+
+use aoc::{parse_memory, IntCode, Result, MemIo};
+
+fn main() -> Result<()> {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf)?;
+
+    let mut mem = parse_memory(&buf)?;
+    extend(&mut mem);
+
+    let mut io = MemIo::new(vec![2]);
+    let computer = IntCode::new(&mut io, &mut mem);
+    computer.run()?;
+    println!("{:?}", io.into_output());
+
+    Ok(())
+}
+
+fn extend(mem: &mut Vec<i64>) {
+    let limit = 64 * 1024;
+    if mem.len() < limit {
+        mem.resize(limit, 0);
+    }
+}
+
+#[test]
+fn test_examples() {
+    fn run(mut prog: Vec<i64>) -> Vec<i64> {
+        extend(&mut prog);
+        let mut io = aio::MemIo::new(vec![]);
+        let computer = IntCode::new(&mut io, &mut prog);
+        computer.run().unwrap();
+        io.into_output()
+    }
+
+    assert_eq!(
+        run(vec![
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
+        ]),
+        vec![109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,]
+    );
+    assert_eq!(
+        run(vec![1102, 34915192, 34915192, 7, 4, 7, 99, 0]),
+        vec![1219070632396864],
+    );
+    assert_eq!(run(vec![104, 1125899906842624, 99]), vec![1125899906842624],)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -78,11 +78,17 @@ pub struct IntCode<'a> {
     io: &'a mut dyn Io,
     mem: &'a mut [i64],
     ip: i64,
+    bp: i64,
 }
 
 impl<'a> IntCode<'a> {
     pub fn new(io: &'a mut dyn Io, mem: &'a mut [i64]) -> IntCode<'a> {
-        IntCode { io, mem, ip: 0 }
+        IntCode {
+            io,
+            mem,
+            ip: 0,
+            bp: 0,
+        }
     }
     pub fn run(mut self) -> Result<()> {
         while self.step()? {}
@@ -93,19 +99,19 @@ impl<'a> IntCode<'a> {
         match op {
             Op::Halt => return Ok(false),
             Op::Arith { op, lhs, rhs, dst } => {
-                let lhs = self.load(lhs)?;
-                let rhs = self.load(rhs)?;
+                let lhs = self.decode_value(lhs)?;
+                let rhs = self.decode_value(rhs)?;
                 let res = op.eval(lhs, rhs);
                 self.store(dst, res)?;
             }
             Op::Jump { op, cond, tgt } => {
-                let cond = self.load(cond)?;
+                let cond = self.decode_value(cond)?;
                 let jump = match op {
                     JumpOp::IfTrue => cond != 0,
                     JumpOp::IfFalse => cond == 0,
                 };
                 if jump {
-                    self.ip = self.load(tgt)?;
+                    self.ip = self.decode_value(tgt)?;
                     return Ok(true);
                 }
             }
@@ -114,63 +120,86 @@ impl<'a> IntCode<'a> {
                 self.store(dst, res)?;
             }
             Op::Output { src } => {
-                let res = self.load(src)?;
+                let res = self.decode_value(src)?;
                 self.io.write(res)?;
+            }
+            Op::AdjBp { adj } => {
+                let adj = self.decode_value(adj)?;
+                self.bp += adj;
             }
         }
         Ok(true)
     }
-    fn load(&self, value: Value) -> Result<i64> {
+
+    fn decode_value(&self, value: Value) -> Result<i64> {
         match value {
             Value::Immediate(it) => Ok(it),
-            Value::Addr(addr) => self.load_addr(addr),
+            Value::Addr(addr) => self.load(addr),
         }
     }
-    fn load_addr(&self, addr: i64) -> Result<i64> {
+    fn decode_addr(&self, addr: Addr) -> Result<usize> {
+        let addr = if addr.rel {
+            self.bp + addr.value
+        } else {
+            addr.value
+        };
         if !(0 <= addr && addr <= self.mem.len() as i64) {
-            Err(format!("invalid addr on load: {}", addr))?
+            Err(format!("invalid addr: {}", addr))?
         }
-        Ok(self.mem[addr as usize])
+        Ok(addr as usize)
     }
-
-    fn store(&mut self, addr: i64, value: i64) -> Result<()> {
-        if !(0 <= addr && addr <= self.mem.len() as i64) {
-            Err(format!("invalid addr on store: {}", addr))?
-        }
-        self.mem[addr as usize] = value;
+    fn load(&self, addr: Addr) -> Result<i64> {
+        let addr = self.decode_addr(addr)?;
+        Ok(self.mem[addr])
+    }
+    fn load_ip(&self) -> Result<i64> {
+        self.load(Addr {
+            value: self.ip,
+            rel: false,
+        })
+    }
+    fn store(&mut self, addr: Addr, value: i64) -> Result<()> {
+        let addr = self.decode_addr(addr)?;
+        self.mem[addr] = value;
         Ok(())
     }
 
     fn decode(&mut self) -> Result<Op> {
         fn to_value(modes: &mut i64, value: i64) -> Result<Value> {
             let res = match *modes % 10 {
-                0 => Value::Addr(value),
+                0 => Value::Addr(Addr { value, rel: false }),
                 1 => Value::Immediate(value),
+                2 => Value::Addr(Addr { value, rel: true }),
                 _ => Err("invalid addr mode")?,
             };
             *modes /= 10;
             Ok(res)
         }
 
-        let op_code = self.load_addr(self.ip)?;
+        let op_code = self.load_ip()?;
         let (mut modes, op_code) = (op_code / 100, op_code % 100);
 
         macro_rules! args {
             ($($m:ident)*) => {{
                 let res = ($(args!(@ $m),)*);
                 if modes != 0 {
-                    Err("invalid add mode")?
+                    Err(format!("leftover modes: {}", modes))?
                 }
                 self.ip += 1;
                 res
             }};
             (@ v) => {{
                 self.ip += 1;
-                to_value(&mut modes, self.load_addr(self.ip)?)?
+                let val = self.load_ip()?;
+                to_value(&mut modes, val)?
             }};
             (@ a) => {{
                 self.ip += 1;
-                self.load_addr(self.ip)?
+                let val = self.load_ip()?;
+                match to_value(&mut modes, val)? {
+                    Value::Addr(it) => it,
+                    Value::Immediate(_) => Err("Immediate address")?,
+                }
             }};
         }
 
@@ -203,6 +232,10 @@ impl<'a> IntCode<'a> {
                 let (cond, tgt) = args!(v v);
                 Op::Jump { op, cond, tgt }
             }
+            9 => {
+                let (adj,) = args!(v);
+                Op::AdjBp { adj }
+            }
             99 => Op::Halt,
             _ => Err(format!("invalid op code: {}", op_code))?,
         };
@@ -210,23 +243,29 @@ impl<'a> IntCode<'a> {
     }
 }
 
-#[derive(Clone, Copy)]
-enum Value {
-    Immediate(i64),
-    Addr(i64),
+#[derive(Debug, Clone, Copy)]
+struct Addr {
+    value: i64,
+    rel: bool,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
+enum Value {
+    Immediate(i64),
+    Addr(Addr),
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Op {
     Halt,
     Arith {
         op: ArithOp,
         lhs: Value,
         rhs: Value,
-        dst: i64,
+        dst: Addr,
     },
     Input {
-        dst: i64,
+        dst: Addr,
     },
     Output {
         src: Value,
@@ -236,9 +275,12 @@ enum Op {
         cond: Value,
         tgt: Value,
     },
+    AdjBp {
+        adj: Value,
+    },
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 enum ArithOp {
     Add,
     Mul,
@@ -257,7 +299,7 @@ impl ArithOp {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 enum JumpOp {
     IfTrue,
     IfFalse,
