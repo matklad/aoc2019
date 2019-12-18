31a732417749aa2fde8cb1ae5b5686bf517a4f65
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-12-18 08:19:33 +0100

    Day 17 wip

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,119 +1,116 @@
-use std::convert::TryFrom;
+use std::{collections::HashSet, convert::TryFrom, hash::Hash};
 
 use aoc::{parse_memory, read_stdin_to_string, Board, Direction, IntCode, MemIo, Point, Result};
 
 fn main() -> Result<()> {
-    let input = read_stdin_to_string()?;
-    let mut mem = parse_memory(&input)?;
-    let mut io = MemIo::new(vec![]);
-    let mut cpu = IntCode::new(&mut io, &mut mem);
-    cpu.run();
+    // let input = read_stdin_to_string()?;
+    // let mut mem = parse_memory(&input)?;
+    // let mut io = MemIo::new(vec![]);
+    // let mut cpu = IntCode::new(&mut io, &mut mem);
+    // cpu.run();
 
-    let output = io.into_output();
-    let width = output.iter().position(|&it| it == b'\n' as i64).unwrap();
-    let height = output.len() / (width + 1);
-    assert_eq!((width + 1) * height + 1, output.len());
-    let board = Board::from_elements(
-        (width, height),
-        output
-            .iter()
-            .map(|&it| u8::try_from(it).unwrap())
-            .filter(|&it| it != b'\n')
-            .map(|it| match it {
-                b'.' | b'^' => Cell::Space,
-                b'#' => Cell::Scaffold,
-                _ => panic!(),
-            }),
-    );
+    // let output = io.into_output();
+    // let width = output.iter().position(|&it| it == b'\n' as i64).unwrap();
+    // let height = output.len() / (width + 1);
+    // assert_eq!((width + 1) * height + 1, output.len());
+    // let board = Board::from_elements(
+    //     (width, height),
+    //     output
+    //         .iter()
+    //         .map(|&it| u8::try_from(it).unwrap())
+    //         .filter(|&it| it != b'\n')
+    //         .map(|it| match it {
+    //             b'.' | b'^' => Cell::Space,
+    //             b'#' => Cell::Scaffold,
+    //             _ => panic!(),
+    //         }),
+    // );
 
-    let res = board
-        .iter()
-        .filter(|(p, cell)| **cell == Cell::Scaffold)
-        .filter(|(p, _)| {
-            Direction::ALL
-                .iter()
-                .map(|it| *p + it.delta())
-                .filter_map(|it| board.get(it))
-                .all(|&it| it == Cell::Scaffold)
-        })
-        .map(|(p, _)| p)
-        .map(|p| p.0 * p.1)
-        .sum::<i64>();
+    // let robot_x = output.iter().position(|&it| it == b'^' as i64).unwrap();
 
-    let robot_x = output.iter().position(|&it| it == b'^' as i64).unwrap();
+    // let mut prev = Point(0, 0);
+    // let mut curr = Point(robot_x as i64, 0);
+    // let mut path = Vec::new();
+    // let mut dir = Direction::Left;
+    // loop {
+    //     let next = [dir, dir.turn(1), dir.turn(2), dir.turn(3)]
+    //         .iter()
+    //         .copied()
+    //         .map(|d| (d, curr + d.delta()))
+    //         .find(|(_d, next)| *next != prev && board.get(*next) == Some(&Cell::Scaffold));
+    //     match next {
+    //         None => break,
+    //         Some((d, next)) => {
+    //             dir = d;
+    //             path.push(dir);
+    //             prev = curr;
+    //             curr = next;
+    //         }
+    //     }
+    // }
 
-    let mut prev = Point(0, 0);
-    let mut curr = Point(robot_x as i64, 0);
-    let mut path = Vec::new();
-    let mut dir = Direction::Left;
-    loop {
-        let next = [dir, dir.turn(1), dir.turn(2), dir.turn(3)]
-            .iter()
-            .copied()
-            .map(|d| (d, curr + d.delta()))
-            .find(|(_d, next)| *next != prev && board.get(*next) == Some(&Cell::Scaffold));
-        match next {
-            None => break,
-            Some((d, next)) => {
-                dir = d;
-                path.push(dir);
-                prev = curr;
-                curr = next;
-            }
-        }
-    }
-
-    eprintln!("path.len() = {:?}", path.len());
-
-    let mut segments = vec![(Direction::Left, 0)];
-    let mut cur_dir = Direction::Left;
-    for &dir in path.iter() {
-        if dir == cur_dir {
-            segments.last_mut().unwrap().1 += 1
-        } else {
-            if cur_dir.turn_left() == dir {
-                cur_dir = dir;
-                segments.push((Direction::Left, 1))
-            } else if cur_dir.turn_right() == dir {
-                cur_dir = dir;
-                segments.push((Direction::Right, 1))
-            } else {
-                panic!()
-            }
-        }
-    }
-
-    for s in segments {
-        eprintln!("{:?}", s);
-    }
-
-    let turns = path
-        .iter()
-        .zip(&path[1..])
-        .filter(|(x, y)| x != y)
-        .map(|(&x, &y)| {
-            if x.turn_left() == y {
-                0
-            } else if x.turn_right() == y {
-                1
-            } else {
-                panic!()
-            }
-        })
-        .collect::<Vec<_>>();
-
-    eprintln!("turns = {:?}", turns);
-
-    // let path = rle(&path);
-    all_segments(&path).map(|seg| (seg, count(&path, seg)));
+    let path = b"RFFFFFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFLFFFFFFLFFRFFFFRFFFFRFFFFFFFFRFFFFFFFFRFFFFFFFFLFFFFFFLFF";
+    let res = triangulate(path);
+    eprintln!("res = {:#?}", res);
 
     Ok(())
 }
 
-fn all_segments<T>(xs: &[T]) -> impl Iterator<Item = &[T]> {
+fn triangulate<T>(xs: &[T]) -> Option<([&[T]; 3], Vec<usize>)>
+where
+    T: Eq + Hash,
+{
+    let segments = interesting_segments(xs);
+    for first_len in 0..xs.len() {
+        let first_seg = &xs[..first_len];
+        if !segments.contains(first_seg) {
+            continue;
+        }
+        for &second_seg in segments.iter() {
+            for &third_seg in segments.iter() {
+                if first_seg == second_seg || first_seg == third_seg || second_seg == third_seg {
+                    continue;
+                }
+                let segs = [first_seg, second_seg, third_seg];
+                if let Some(idxes) = try_triangulate(xs, segs) {
+                    return Some((segs, idxes));
+                }
+            }
+        }
+    }
+    None
+}
+
+fn try_triangulate<'a, T>(mut xs: &'a [T], segs: [&'a [T]; 3]) -> Option<Vec<usize>>
+where
+    T: Eq,
+{
+    let mut res = Vec::new();
+    'outer: while !xs.is_empty() {
+        for i in 0..3 {
+            if xs.starts_with(segs[i]) {
+                xs = &xs[segs[i].len()..];
+                res.push(i);
+                continue 'outer;
+            }
+        }
+        return None;
+    }
+    Some(res)
+}
+
+fn interesting_segments<T>(xs: &[T]) -> HashSet<&[T]>
+where
+    T: Eq + Hash,
+{
+    let min_len = 3;
+    let min_repeat = 3;
+
     (0..xs.len())
-        .flat_map(move |i| (i + 2..xs.len()).map(move |j| (i, j)))
+        .flat_map(move |i| (i + min_len..xs.len()).map(move |j| (i, j)))
         .map(move |(i, j)| &xs[i..j])
+        .filter(|seg| count(xs, seg) >= min_repeat)
+        .collect()
 }
 
 fn count<T: Eq>(haystack: &[T], needle: &[T]) -> usize {
